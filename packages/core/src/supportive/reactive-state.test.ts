200ad1933058ded6d682ad4ca964fe65e1de3aea
marhali <me@marhali.de>
2025-02-03 23:25:50 +0100

    feat(supportive): implement trackable state manager

---
+++ packages/core/src/supportive/reactive-state.test.ts
@@ -0,0 +1,37 @@
+import { describe, it, expect, vi, beforeEach } from 'vitest';
+import createReactiveState, { ReactiveState } from '@depjack/core/supportive/reactive-state.ts';
+
+describe('reactive-state', () => {
+  describe('createReactiveState()', () => {
+    type Type = { string: string; number: number; boolean: boolean };
+    let initialState: Type;
+    let state: ReactiveState<Type>;
+    beforeEach(() => {
+      initialState = { string: 'myInitialString', number: 0, boolean: false };
+      state = createReactiveState<Type>(initialState);
+    });
+    it('should set first state by provided initialState param', () => {
+      expect(state.get('string')).toBe(initialState.string);
+      expect(state.get('number')).toBe(initialState.number);
+      expect(state.get('boolean')).toBe(initialState.boolean);
+    });
+    it('should override property by set() function and notify any registered listeners on this key', () => {
+      const listener = vi.fn();
+      state.subscribe('string', listener);
+      expect(state.get('string')).toBe(initialState.string);
+      state.set('string', 'myNewValue');
+      expect(state.get('string')).toBe('myNewValue');
+      expect(listener).toHaveBeenCalledTimes(1);
+      expect(listener).toHaveBeenLastCalledWith('myNewValue');
+    });
+    it('should return teardown function on subscribe function to unregister the listener', () => {
+      const listener = vi.fn();
+      const teardownFunction = state.subscribe('number', listener);
+      state.set('number', 1);
+      teardownFunction();
+      state.set('number', 2);
+      expect(listener).toHaveBeenCalledTimes(1);
+      expect(listener).toHaveBeenLastCalledWith(1);
+    });
+  });
+});
---
+++ packages/core/src/supportive/reactive-state.ts
@@ -0,0 +1,35 @@
+import createPubSub from '@depjack/core/supportive/pub-sub.ts';
+
+/**
+ * Represents a tracked state of key / value pairs.
+ */
+export type ReactiveState<T> = {
+  /** Returns state value by key. */
+  get: <Key extends keyof T>(key: Key) => Readonly<T[Key]>;
+
+  /** Overrides a specified state value by key. */
+  set: <Key extends keyof T, Value extends T[Key]>(key: Key, value: Value) => void;
+
+  /** Registers a new listener that listens on the specified state key. */
+  subscribe: <Key extends keyof T>(key: Key, listener: (payload: T[Key]) => void) => () => void;
+};
+
+/**
+ * Creates a new tracked state instance.
+ * @param initialState Initial state to apply
+ */
+function createReactiveState<T>(initialState: T): ReactiveState<T> {
+  const state: T = initialState;
+  const pubSub = createPubSub<T>();
+
+  return {
+    get: (key) => state[key],
+    set: (key, value) => {
+      state[key] = value;
+      pubSub.publish(key, value);
+    },
+    subscribe: (key, listener) => pubSub.subscribe(key, listener),
+  };
+}
+
+export default createReactiveState;
