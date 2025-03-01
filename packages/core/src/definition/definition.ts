b33744f60f95e09a5122a1762afc468243a53a8e
marhali <me@marhali.de>
2025-03-01 19:05:43 +0100

    refactor(core): prefix generic types with T to improve readability

--- packages/core/src/definition/definition.ts
+++ packages/core/src/definition/definition.ts
@@ -7,31 +7,31 @@ export type Deps = Record<string, unknown>;
  * Maps the provided dependencies as lazily loaded using an async function.
  * @see Deps
  */
-export type DepsLazyFunction<T extends Deps> = {
-  [K in keyof T]: () => Promise<T[K]>;
+export type DepsLazyFunction<TDeps extends Deps> = {
+  [K in keyof TDeps]: () => Promise<TDeps[K]>;
 };
 
 /**
  * Record of dependencies instances. Allows empty keys.
  */
-export type DepsInstance<T extends Deps> = {
-  [K in keyof T]?: T[K];
+export type DepsInstance<TDeps extends Deps> = {
+  [K in keyof TDeps]?: TDeps[K];
 };
 
 /**
  * Record of lazily loaded instances. Allows empty keys.
  */
-export type DepsLazyInstance<T extends Deps> = {
-  [K in keyof T]?: Promise<T[K]>;
+export type DepsLazyInstance<TDeps extends Deps> = {
+  [K in keyof TDeps]?: Promise<TDeps[K]>;
 };
 
 /**
  * Represents any dependency key identifier.
  */
-export type DepsKey<T extends Deps> = keyof T;
+export type DepsKey<TDeps extends Deps> = keyof TDeps;
 
-export type DepsDefinition<T extends Deps> = {
-  [K in keyof T]: {
+export type DepsDefinition<TDeps extends Deps> = {
+  [K in keyof TDeps]: {
     /**
      * Defines whether this dependency should be initialized on-demand (true) or
      * with the initialization of the runtime environment (false).
@@ -42,12 +42,17 @@ export type DepsDefinition<T extends Deps> = {
      * Defines the required dependencies of this dependency.
      * The runtime ensures that these dependencies are available in the factory function.
      */
-    needs: DepsKey<T>[];
+    needs: DepsKey<TDeps>[];
 
     /**
      * Defines the lazy required dependencies of this dependency.
      * The runtime will provide these dependencies to the factory function but won't initialize them.
      */
-    needsLazy: DepsKey<T>[];
+    needsLazy: DepsKey<TDeps>[];
   };
 };
+
+export type PartialDepsDefinition<DEPS extends Deps, PARTIAL extends Partial<DEPS>> = Pick<
+  DepsDefinition<DEPS>,
+  Extract<keyof PARTIAL, keyof DEPS>
+>;
