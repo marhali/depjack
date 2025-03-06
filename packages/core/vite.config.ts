18f32ff4d328481ed0560f36f1c68d215463de4c
marhali <me@marhali.de>
2025-03-06 18:08:45 +0100

    feat(core): configure exported modules

--- packages/core/vite.config.ts
+++ packages/core/vite.config.ts
@@ -1,9 +1,20 @@
 /// <reference types="vitest" />
 import { defineConfig } from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
+import dtsPlugin from 'vite-plugin-dts';
 
 export default defineConfig({
-  plugins: [tsconfigPaths()],
+  plugins: [tsconfigPaths(), dtsPlugin({ rollupTypes: true })],
+  build: {
+    minify: false,
+    lib: {
+      entry: {
+        definition: 'src/definition/index.ts',
+        factory: 'src/factory/index.ts',
+        runtime: 'src/runtime/index.ts',
+      },
+    },
+  },
   test: {
     // ...
   },
