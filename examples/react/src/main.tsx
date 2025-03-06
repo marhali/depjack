6d9646b3c914eb3bce63d41854213e6d0c5d4d9b
marhali <me@marhali.de>
2025-03-06 23:48:12 +0100

    fix: linting

--- examples/react/src/main.tsx
+++ examples/react/src/main.tsx
@@ -1,7 +1,9 @@
+/* eslint-disable unicorn/no-abusive-eslint-disable */
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import Bootstrap from '~/examples/react/bootstrap/presentation/bootstrap';
 
+// eslint-disable-next-line
 createRoot(document.querySelector('#root')!).render(
   <StrictMode>
     <Bootstrap />
