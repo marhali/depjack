23bcff4baf12a71b5b67e686f58a2006146003a9
marhali <me@marhali.de>
2025-03-06 18:07:12 +0100

    refactor: use index modules to provide library api

---
+++ packages/core/src/runtime/index.ts
@@ -0,0 +1,1 @@
+export { default as createDepsRuntime } from '@depjack/core/runtime/create-deps-runtime';
---
+++ packages/react/src/index.ts
@@ -0,0 +1,3 @@
+export { default as useBootstrapDepsRuntime } from '@depjack/react/use-bootstrap-deps-runtime';
+export { default as useInitializingDeps } from '@depjack/react/use-initializing-deps';
+export { default as useResolveDep } from '@depjack/react/use-resolve-dep';
