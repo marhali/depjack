2407be96b00fb5210bc576719f293a970f89ac13
marhali <me@marhali.de>
2025-03-06 22:03:05 +0100

    build: improve tsconfig resolution

--- eslint.config.js
+++ eslint.config.js
@@ -6,45 +6,30 @@ import eslintPluginVitest from '@vitest/eslint-plugin';
 import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
 
 export default typescriptEslint.config(
-  // eslint-disable-next-line @typescript-eslint/no-unsafe-argument,@typescript-eslint/no-unsafe-member-access
   eslintJs.configs.recommended,
-  // eslint-disable-next-line @typescript-eslint/no-unsafe-argument,@typescript-eslint/no-unsafe-member-access
-  eslintPluginImport.flatConfigs.recommended,
-  {
-    settings: {
-      'import/resolver': {
-        typescript: {
-          project: 'tsconfig*(.*).json',
-        },
-        node: true,
-      },
-    },
-  },
   typescriptEslint.configs.recommendedTypeChecked,
   {
     languageOptions: {
       parserOptions: {
-        projectService: {
-          allowDefaultProject: ['eslint.config.js'],
-        },
-        // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+        project: ['tsconfig.base.json', './packages/*/tsconfig.json', './examples/*/tsconfig.json'],
         tsconfigRootDir: import.meta.dirname,
       },
     },
+  },
+  {
+    files: ['*.{ts,tsx}'],
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+    extends: [eslintPluginImport.flatConfigs.recommended, eslintPluginImport.flatConfigs.typescript],
   },
   eslintPluginUnicorn.configs['flat/recommended'],
   {
     files: ['*.test.*'],
     plugins: {
-      // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
       vitest: eslintPluginVitest,
     },
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
     rules: {
-      // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
       ...eslintPluginVitest.configs.recommended.rules,
     },
   },
-  // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
   eslintPluginPrettierRecommended,
 );
