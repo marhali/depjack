913b6a9f0b7bbfaf72a389c284c1253dcd3f7219
marhali <me@marhali.de>
2025-03-06 23:48:05 +0100

    build: fix import resolver

--- eslint.config.js
+++ eslint.config.js
@@ -5,25 +5,38 @@ import eslintPluginUnicorn from 'eslint-plugin-unicorn';
 import eslintPluginVitest from '@vitest/eslint-plugin';
 import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
 
+const project = ['tsconfig.base.json', './packages/*/tsconfig.json', './examples/*/tsconfig.json'];
+
 export default typescriptEslint.config(
+  {
+    ignores: ['**/dist/**'],
+  },
   eslintJs.configs.recommended,
   typescriptEslint.configs.recommendedTypeChecked,
   {
     languageOptions: {
       parserOptions: {
-        project: ['tsconfig.base.json', './packages/*/tsconfig.json', './examples/*/tsconfig.json'],
+        project,
         tsconfigRootDir: import.meta.dirname,
       },
     },
   },
   {
-    files: ['*.{ts,tsx}'],
+    files: ['**/*.{ts,tsx}'],
     // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
     extends: [eslintPluginImport.flatConfigs.recommended, eslintPluginImport.flatConfigs.typescript],
+    settings: {
+      'import/resolver': {
+        node: true,
+        typescript: {
+          project,
+        },
+      },
+    },
   },
   eslintPluginUnicorn.configs['flat/recommended'],
   {
-    files: ['*.test.*'],
+    files: ['**/*.test.*'],
     plugins: {
       vitest: eslintPluginVitest,
     },
