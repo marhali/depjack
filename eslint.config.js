380a53a7ae380d63b3a10947814b1077b95e3cf6
marhali <code@shield.marhali.de>
2025-09-03 20:56:59 +0200

    fix: browser lib type support

--- eslint.config.js
+++ eslint.config.js
@@ -1,20 +1,24 @@
+// @ts-check
+
+import globals from 'globals';
 import eslintJs from '@eslint/js';
 import typescriptEslint from 'typescript-eslint';
 import eslintPluginImport from 'eslint-plugin-import';
 import eslintPluginUnicorn from 'eslint-plugin-unicorn';
 import eslintPluginVitest from '@vitest/eslint-plugin';
 import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
+import { defineConfig, globalIgnores } from 'eslint/config';
 
 const project = ['tsconfig.base.json', './packages/*/tsconfig.json', './examples/*/tsconfig.json'];
 
-export default typescriptEslint.config(
-  {
-    ignores: ['**/dist/**'],
-  },
+export default defineConfig(
+  globalIgnores(['**/dist']),
   eslintJs.configs.recommended,
   typescriptEslint.configs.recommendedTypeChecked,
   {
     languageOptions: {
+      ecmaVersion: 2022,
+      globals: globals.browser,
       parserOptions: {
         project,
         tsconfigRootDir: import.meta.dirname,
@@ -23,7 +27,6 @@ export default typescriptEslint.config(
   },
   {
     files: ['**/*.{ts,tsx}'],
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
     extends: [eslintPluginImport.flatConfigs.recommended, eslintPluginImport.flatConfigs.typescript],
     settings: {
       'import/resolver': {
